d083731aac26e8bf9911e459538fa25c3f80c9c1
Abhiraj2501 <pinujawalkar1925@gmail.com>
2026-06-01 23:11:50 +0530

    auto: 2026-05-21 20:43:02

--- ChatModels/1_chatmodel_openai.py
+++ ChatModels/1_chatmodel_openai.py
@@ -10,31 +10,3 @@ result = model.invoke("Define LCM meta research")
 
 print(result.content)
 
-# Simple LangChain Chatbot using OpenAI
-
-# Install first:
-# pip install langchain langchain-openai python-dotenv
-
-from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
-from langchain_core.messages import HumanMessage
-
-# Load API key from .env file
-load_dotenv()
-
-# Initialize model
-llm = ChatOpenAI(
-    model="gpt-4.1-mini",
-    temperature=0.7
-)
-
-# User input
-user_input = input("You: ")
-
-# Send message
-response = llm.invoke([
-    HumanMessage(content=user_input)
-])
-
-# Print response
-print("\nAI:", response.content)
