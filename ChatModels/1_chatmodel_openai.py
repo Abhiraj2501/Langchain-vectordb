a4be5248bb1ebb06de7cbf4dd8d25d055a119d1a
Abhiraj2501 <pinujawalkar1925@gmail.com>
2026-06-01 23:11:50 +0530

    auto: 2026-05-18 02:49:55

--- ChatModels/1_chatmodel_openai.py
+++ ChatModels/1_chatmodel_openai.py
@@ -5,6 +5,7 @@ load_dotenv()
 
 model = ChatOpenAI(model='gpt-4', temperature=1.5, max_completion_tokens=10)
 #temperature is a parameter that controls the randomness of the output. Higher values (e.g., 1.5) will make the output more random, while lower values (e.g., 0.2) will make it more focused and deterministic.
+
 result = model.invoke("Define LCM meta research")
 
 print(result.content)
