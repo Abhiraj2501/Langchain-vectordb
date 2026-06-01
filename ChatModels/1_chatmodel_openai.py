03b5d460edf18838e0d261584b8bfb74905a0f2d
Abhiraj2501 <pinujawalkar1925@gmail.com>
2026-06-01 23:11:50 +0530

    auto: 2026-05-29 11:45:49

--- ChatModels/1_chatmodel_openai.py
+++ ChatModels/1_chatmodel_openai.py
@@ -8,4 +8,4 @@ model = ChatOpenAI(model='gpt-4', temperature=1.5, max_completion_tokens=10)
 #max_completion_tokens is a parameter that limits the number of tokens in the generated response. Setting it to a low value (e.g., 10) will result in shorter responses, while a higher value (e.g., 100) will allow for more detailed answers.
 result = model.invoke("Define LCM meta research")
 
-print(result.content)
+print(result.content) 
