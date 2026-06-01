b0d4ddc4993ce1592438fba5c877c9ec34c57e1c
Abhiraj2501 <pinujawalkar1925@gmail.com>
2026-06-01 23:11:50 +0530

    auto: 2026-05-18 02:46:51

--- ChatModels/1_chatmodel_openai.py
+++ ChatModels/1_chatmodel_openai.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 model = ChatOpenAI(model='gpt-4', temperature=1.5, max_completion_tokens=10)
-
+#temp
 result = model.invoke("Define LCM meta research")
 
 print(result.content)
