76e9243c6f0e067498b4256cc57bd186d69e7118
Abhiraj2501 <pinujawalkar1925@gmail.com>
2026-06-01 23:11:50 +0530

    auto: 2026-05-18 02:44:12

--- ChatModels/1_chatmodel_openai.py
+++ ChatModels/1_chatmodel_openai.py
@@ -5,6 +5,6 @@ load_dotenv()
 
 model = ChatOpenAI(model='gpt-4', temperature=1.5, max_completion_tokens=10)
 
-result = model.invoke("Write a 5 line poem on cricket")
+result = model.invoke("Define LCM ")
 
 print(result.content)
