8512ffa879e4eeca952e8a830c526b8e54c1ac5b
Abhiraj2501 <pinujawalkar1925@gmail.com>
2026-06-01 23:11:50 +0530

    auto: 2026-05-18 02:49:58

--- ChatModels/1_chatmodel_openai.py
+++ ChatModels/1_chatmodel_openai.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 model = ChatOpenAI(model='gpt-4', temperature=1.5, max_completion_tokens=10)
 #temperature is a parameter that controls the randomness of the output. Higher values (e.g., 1.5) will make the output more random, while lower values (e.g., 0.2) will make it more focused and deterministic.
-
+#max_completion_tokens is a parameter that limits the number of tokens in the generated response. Setting it to a low value (e.g., 10) will result in shorter responses, while a higher value (e.g., 100) will allow for more detailed answers.
 result = model.invoke("Define LCM meta research")
 
 print(result.content)
