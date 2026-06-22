719ec93b1c35ed88f6bebc9f9a170b4ecac6153e
Abhiraj2501 <pinujawalkar1925@gmail.com>
2026-06-22 23:59:26 +0530

    auto: 2026-06-22 23:59:26

--- ChatModels/2_chatmodel_anthropic.py
+++ ChatModels/2_chatmodel_anthropic.py
@@ -11,4 +11,12 @@ result = model.invoke('What is the capital of India')
 print(result.content)
 
 def test_chat_model_anthropic():
+    model = ChatAnthropic(model='claude-3-5-sonnet-20241022')
+    result = model.invoke('What is the capital of India')
+    assert result.content is not None
+    assert result.metadata is not None
+    assert result.tokens is not None
+    assert result.usage is not None
+    assert result.cost is not None
+    assert result.model == 'claude-3-5-sonnet-20241022'
     
